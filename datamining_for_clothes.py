7ea29e86dd3870410bb6c3dea4bbe168e200f576
Bongjun Choi <bongjun2.choi@gmail.com>
2023-03-17 10:26:16 +0900

    add example code

--- datamining_for_clothes.py
+++ datamining_for_clothes.py
@@ -3,14 +3,21 @@ import numpy as np
 
 df = pd.read_excel("bestselling.xlsx")
 # print(df)
+
 print(df.head(3))
 
 # df['site']=df['관리번호(사이트-YYYY-MM-W-NN)'].str[:2]
 df['ym'] = df['관리번호(사이트-YYYY-MM-W-NN)'].str[3:11]
+# df['y'] = df['관리번호(사이트-YYYY-MM-W-NN)'].str[3:7]
+# df['m'] = df['관리번호(사이트-YYYY-MM-W-NN)'].str[8:11]
 # print(df.head(3))
 
+# print(df.groupby('ym')['카테고리'].value_counts())
 
-# print(df.groupby('아이템')['기장'].value_counts())
+# print(df.groupby(['ym', '카테고리'])['기장'].value_counts())
 
-print(df.groupby('ym')['카테고리'].value_counts())
-print(df.groupby(['ym','카테고리'])['기장'].value_counts())
+# print(df.groupby(['ym', '카테고리', '기장'])['네크라인'].value_counts())
+
+
+
+print(df.groupby('아이템')['기장'].value_counts())
